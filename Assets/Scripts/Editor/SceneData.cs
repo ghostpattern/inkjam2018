e06ecee9ae8aa8cb68ebc6fe80939a0c03f55b9b
Thomas Ingram <thomasingram@vertx.xyz>
2018-09-01 16:54:16 +1000

    Added SceneData for all scenes

---
+++ Assets/Scripts/Editor/SceneData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// An Editor-Time asset for loading Scenes. A runtime version will need to be built.
+/// </summary>
+[CreateAssetMenu(fileName = "Scene.asset", menuName = "Scene Data", order = 0)]
+public class SceneData : ScriptableObject
+{
+	public List<SceneAsset> sceneList;
+
+	/// <summary>
+	/// Function called when a user tries to open a file, we discover whether this was a Loader and therefore whether it should be loaded
+	/// </summary>
+	[OnOpenAsset(1)]
+	public static bool TryToLoadAssets(int instanceID, int line)
+	{
+		Object @this = EditorUtility.InstanceIDToObject(instanceID);
+		SceneData loader = @this as SceneData;
+		if (loader == null)
+			return false;
+		
+		//We currently are not going to support runtime loading of Loaders.
+		if (Application.isPlaying)
+		{
+			Debug.LogWarning("Loading Loaders at Runtime is not currently supported.");
+			return true;
+		}
+
+		for (int i = 0; i < loader.sceneList.Count; i++)
+		{
+			SceneAsset sceneAsset = loader.sceneList[i];
+			EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(sceneAsset), i == 0 ? OpenSceneMode.Single : OpenSceneMode.Additive);
+		}
+
+		return true;
+	}
+}
---
+++ Assets/Scripts/Editor/SceneDataEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+[CustomEditor(typeof(SceneData))]
+public class SceneDataEditor : Editor
+{
+	private ReorderableList _reorderableList;
+	private void OnEnable()
+	{
+		SerializedProperty sP = serializedObject.FindProperty("sceneList");
+		_reorderableList = new ReorderableList(serializedObject, sP)
+		{
+			drawHeaderCallback = rect => EditorGUI.LabelField(rect, "Scenes", EditorStyles.boldLabel),
+			drawElementCallback = (rect, index, active, focused) => EditorGUI.ObjectField(rect, sP.GetArrayElementAtIndex(index), GUIContent.none),
+			elementHeight = EditorGUIUtility.singleLineHeight
+		};
+
+	}
+
+	public override void OnInspectorGUI()
+	{
+		_reorderableList.DoLayoutList();
+	}
+}
