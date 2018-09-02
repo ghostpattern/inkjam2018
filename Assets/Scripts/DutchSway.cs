3ade73c2156a4da879fca64f989ab584f41c0ddd
Thomas Ingram <thomasingram@vertx.xyz>
2018-09-02 21:54:16 +1000

    Added sway to the Bridge scene to make the view more dynamic

---
+++ Assets/Scripts/DutchSway.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class DutchSway : MonoBehaviour
+{
+
+	[ReadOnly] public CinemachineVirtualCamera cinemachineVirtualCamera;
+
+	// Use this for initialization
+	private void Reset()
+	{
+		cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+	}
+
+	public float speed = 1;
+	private float time;
+	public float dutch = 1;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		time += Time.deltaTime * speed;
+		cinemachineVirtualCamera.m_Lens.Dutch = Mathf.Sin(time) * dutch;
+	}
+}
